e150da1725f50673eba666a064420b198e55a7e0
bigboss121212 <caron.alex18@hotmail.fr>
2023-01-30 13:01:18 -0500

    entrainement

--- TP1/prediction.py
+++ TP1/prediction.py
@@ -1,46 +1,13 @@
-from sys import argv
-import re
-
 class Prediction:
-    def __init__(self, chemin, encodage = 'utf-8'):
-        self.chemin = chemin
-        self.encodage = encodage
-        self.text = None
-
-    def lire(self):
-        f = open(self.chemin, 'r', encoding=self.encodage)
-        text = f.read()
-        f.close()
-        return text
-
-    def compter_mots(self):
-        return len(re.findall('/w+', self.text))
-
-    def retirer_mots(self, texte):
-        d = {}
-        mots = re.split(' |\n|\.|\?', texte)
-        list = ' '.join(mots).split()
-
-        new_list = []
-        index = 0
-        for i in list:
-            if i not in new_list:
-                d.update({index : i})
-                new_list.append(i)
-                index += 1
-
-        print(d)
-
+    def __init__(self):
+        pass
 
 
 def main():
-    chemin = argv[1]
-    #enc = argv[2]
-    #souschaine = argv[3]
-    prediction = Prediction(chemin)
-    text = prediction.lire()
-    prediction.retirer_mots(text)
+    pass
 
 
 if __name__ == '__main__':
     quit(main())
+
+
