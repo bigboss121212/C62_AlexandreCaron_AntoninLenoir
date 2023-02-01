65a22d8ca775970c0c1658712a786cba89c87bff
bigboss121212 <caron.alex18@hotmail.fr>
2023-01-31 21:07:34 -0500

    liste de phrase

--- TP1/entrainement.py
+++ TP1/entrainement.py
@@ -9,11 +9,13 @@ class Entrainement:
         self.text = None
         self.matrice = None
         self.new_list = []
+        self.file_path = r"X:\Session6\Donnees, Megadonnees, Intelligence Artificielle\semaine2\test.txt"
 
     def lire(self):
         f = open(self.chemin, 'r', encoding=self.encodage)
         text = f.read()
         f.close()
+        self.text = text
         return text
 
     def compter_mots(self):
@@ -24,9 +26,9 @@ class Entrainement:
         mots = re.split(' |\n|\.|\?', texte)
         list = ' '.join(mots).split()
 
-
         index = 0
         for i in list:
+            #pour enlever les doublons
             if i not in self.new_list:
                 d.update({i : index})
                 self.new_list.append(i)
@@ -39,19 +41,27 @@ class Entrainement:
         print(self.matrice)
 
     def remplir_matrice(self):
-
+        sentences = []
+        for i in self.new_list:
+            #faire une liste de phrase
+            #chatgp regarder la focntion strip()
+            sentences = re.split(r'[.!?]+', self.text)
+            sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+            sentences
 
 
         pass
 
 
 def main():
-    chemin = argv[1]
+    chemin = r"X:\Session6\Donnees, Megadonnees, Intelligence Artificielle\semaine2\test.txt"
+    # chemin = argv[1]
     #enc = argv[2]
     #souschaine = argv[3]
     entrainement = Entrainement(chemin)
     text = entrainement.lire()
     entrainement.retirer_mots(text)
+    entrainement.remplir_matrice()
 
 
 if __name__ == '__main__':
