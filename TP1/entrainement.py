64b22dd18827c540c7f9aa968534ff502c3eba72
bigboss121212 <caron.alex18@hotmail.fr>
2023-02-01 11:39:17 -0500

    merde

--- TP1/entrainement.py
+++ TP1/entrainement.py
@@ -8,8 +8,10 @@ class Entrainement:
         self.encodage = encodage
         self.text = None
         self.matrice = None
+        self.dictionnaire = None
         self.new_list = []
         self.file_path = r"X:\Session6\Donnees, Megadonnees, Intelligence Artificielle\semaine2\test.txt"
+        self.fenetre = 7
 
     def lire(self):
         f = open(self.chemin, 'r', encoding=self.encodage)
@@ -35,6 +37,7 @@ class Entrainement:
                 index += 1
 
         print(d)
+        self.dictionnaire = d
 
         self.matrice = np.zeros((len(self.new_list), len(self.new_list)))
 
@@ -42,19 +45,64 @@ class Entrainement:
 
     def remplir_matrice(self):
         sentences = []
-        for i in self.new_list:
+        #for i in self.new_list:
             #faire une liste de phrase
             #chatgp regarder la focntion strip()
-            sentences = re.split(r'[.!?]+', self.text)
-            sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
-            sentences
+        sentences = re.split(' |\n|\.|\?', self.text)
+        list = ' '.join(sentences).split()
+
+
+        list
+        #sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+
+        for i in range(len(list)):
+            i
+            if i > 0:
+                list[i - 1]
+                self.dictionnaire
+                self.dictionnaire[list[i - 1]]
+                if self.dictionnaire[list[i]] != self.dictionnaire[list[i - 1]]:
+                    self.matrice[self.dictionnaire[list[i]],self.dictionnaire[list[i - 1]]] += 1
+            if i > 1:
+                list[i - 2]
+                self.dictionnaire
+                self.dictionnaire[list[i - 2]]
+                if self.dictionnaire[list[i]] != self.dictionnaire[list[i - 2]]:
+                    self.matrice[self.dictionnaire[list[i]], self.dictionnaire[list[i - 2]]] += 1
+            if i > 2:
+                list[i - 3]
+                self.dictionnaire
+                self.dictionnaire[list[i - 3]]
+                if self.dictionnaire[list[i]] != self.dictionnaire[list[i - 3]]:
+                    self.matrice[self.dictionnaire[list[i]], self.dictionnaire[list[i - 3]]] += 1
+            if i < len(list) - 1:
+                list[i + 1]
+                self.dictionnaire[list[i + 1]]
+                if self.dictionnaire[list[i]] != self.dictionnaire[list[i + 1]]:
+                    self.matrice[self.dictionnaire[list[i]], self.dictionnaire[list[i + 1]]] += 1
+            if i < len(list) - 2:
+                list[i + 2]
+                self.dictionnaire[list[i + 2]]
+                if self.dictionnaire[list[i]] != self.dictionnaire[list[i + 2]]:
+                    self.matrice[self.dictionnaire[list[i]], self.dictionnaire[list[i + 2]]] += 1
+            if i < len(list) - 3:
+                list[i + 3]
+                self.dictionnaire[list[i + 3]]
+                if self.dictionnaire[list[i]] != self.dictionnaire[list[i + 3]]:
+                    self.matrice[self.dictionnaire[list[i]], self.dictionnaire[list[i + 3]]] += 1
+
+
+            #for j in sentences:
+            #    phrase = j
+            #    list_mots_phrase = re.split(' ', phrase)
+        self.matrice
 
 
         pass
 
 
 def main():
-    chemin = r"X:\Session6\Donnees, Megadonnees, Intelligence Artificielle\semaine2\test.txt"
+    chemin = r"X:\Session6\Donnees, Megadonnees, Intelligence Artificielle\semaine2\test2.txt"
     # chemin = argv[1]
     #enc = argv[2]
     #souschaine = argv[3]
